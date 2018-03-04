1df7788dc8882b78a6d9566727127ace6d9d9c98
bzibzibzi666 <bzibzibzi666@wp.pl>
2018-03-04 18:40:05 +0100

    Add comic book and artist models

---
+++ src/MVCBasic.ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCBasic.ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ src/MVCBasic.ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBasic.ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Issue { get; set; }
+        public string DescriptionHtml { get; set; }
+        public List<Artist> Artists { get; set; }
+        public bool Fivorite { get; set; }
+    }
+}
