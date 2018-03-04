f0b91bd235bb9aea441a0820d8e86ba2d58a5482
bzibzibzi666 <bzibzibzi666@wp.pl>
2018-03-04 20:34:59 +0100

    updated comic book controler - details by id

--- src/MVCBasic.ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/MVCBasic.ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,26 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCBasic.ComicBookGallery.Data;
 
 namespace MVCBasic.ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        public ActionResult Detail(int? id)
         {
-            var comicBook = new ComicBook()
-            {
-                Title = "The Amazing Spider-Man ",
-                Issue = 700,
-                DescriptionHtml = "<p>Description</p>",
-                Artists = new List<Artist>()
-                {
-                    new Artist(){Name="Name One", Role="Role One"},
-                    new Artist(){Name="Name Two", Role="Role Two"},
-                    new Artist(){Name="NameThree", Role="Role Three"}
-                }
-            };
-            return View(comicBook);
+            if (id == null)
+                return HttpNotFound();
+            return View(ComicBookRepository.GetComicBook((int)id));
         }
     }
 }
---
+++ src/MVCBasic.ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,64 @@
+﻿using MVCBasic.ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBasic.ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        public static ComicBook GetComicBook(int id) => GetComicBooks().Where(x => x.Id == id).SingleOrDefault();
+
+        public static List<ComicBook> GetComicBooks()
+        {
+            return new List<ComicBook>()
+            {
+                new ComicBook()
+                {
+                    Id = 1,
+                    Title = "The Amazing Spider-Man ",
+                    Issue = 700,
+                    DescriptionHtml = "<p>Description</p>",
+                    Artists = new List<Artist>()
+                    {
+                        new Artist(){Name="Name One", Role="Role One"},
+                        new Artist(){Name="Name Two", Role="Role Two"},
+                        new Artist(){Name="NameThree", Role="Role Three"}
+                    }
+                },
+                new ComicBook()
+                {
+                    Id = 2,
+                    Title = "The Amazing Spider-Man ",
+                    Issue = 657,
+                    DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
+                    Artists = new List<Artist>
+                    {
+                        new Artist() { Name = "Dan Slott", Role = "Script" },
+                        new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                        new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                        new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                        new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                    },
+                    Favorite = false
+                },
+                new ComicBook()
+                {
+                    Id = 3,
+                    Title = "Bone ",
+                    Issue= 50,
+                    DescriptionHtml = "<p><strong>The Dungeon & The Parapet, Part 1.</strong> Thorn is discovered by Lord Tarsil and the corrupted Stickeaters and thrown into a dungeon with Fone Bone. As she sleeps, a message comes to her about the mysterious \"Crown of Horns\".</p>",
+                    Artists = new List<Artist>
+                    {
+                        new Artist() { Name = "Jeff Smith", Role = "Script" },
+                        new Artist() { Name = "Jeff Smith", Role = "Pencils" },
+                        new Artist() { Name = "Jeff Smith", Role = "Inks" },
+                        new Artist() { Name = "Jeff Smith", Role = "Letters" }
+                    },
+                    Favorite = false
+                }
+            };
+        }
+    }
+}
